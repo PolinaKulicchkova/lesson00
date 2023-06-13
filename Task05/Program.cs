15efd2aa58533b4dbdc6566e560eb68d1115169b
PolinaKulicchkova <polya841345@gmail.com>
2023-06-13 21:11:23 +0300

    Added Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+int i = -number;
+while(i <= number)
+{
+    Console.WriteLine(i);
+    ++i;
+}
